2bd0b82b7386c8113c329585ce41fbadb4b76bc8
G Gordon Worley III <gworley3@gmail.com>
2015-01-10 01:26:29 +0000

    Switch to speclj

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject monacella "0.1.0-SNAPSHOT"
   :description "The Accidental Distributed Multihypergraph Database"
-  :url "https://github.com/gworley3/monacella/"
+  :url "http://gworley3.github.io/monacella/"
   :license {:name "MIT License"
             :url "https://github.com/gworley3/monacella/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot monacella.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[speclj "3.1.0"]]
+  :test-paths ["spec"]
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [
+               [speclj "3.1.0"]]}})
---
+++ spec/monacella/core_spec.clj
@@ -0,0 +1,5 @@
+(ns monacella.core-spec
+  (:require [speclj.core :refer :all]
+            [monacella.core :refer :all]))
+
+(describe "monacella.core/")
